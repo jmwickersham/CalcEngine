a02416107f3114f3996c5ba1e14e7929ee559ec4
Jennifer Wickersham <jennifermwickersham@gmail.com>
2018-11-21 21:48:34 -0800

    adding main classes

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 		// ** Iteration 4 **
-		MathEquation[] equations = new MathEquation[4];
-		equations[0] = new MathEquation('d', 100.0d, 50.0d);
-		equations[1] = new MathEquation('a', 25.0d, 92.0d);
-		equations[2] = new MathEquation('s', 225.0d, 17.0d);
-		equations[3] = new MathEquation('m', 11.0d, 3.0d);
+		//		MathEquation[] equations = new MathEquation[4];
+		//		equations[0] = new MathEquation('d', 100.0d, 50.0d);
+		//		equations[1] = new MathEquation('a', 25.0d, 92.0d);
+		//		equations[2] = new MathEquation('s', 225.0d, 17.0d);
+		//		equations[3] = new MathEquation('m', 11.0d, 3.0d);
 
     	// ** Iteration 3 **
 
@@ -18,35 +18,53 @@ public class Main {
 		//	equations[2] = create(225.0d, 17.0d, 's');
 		//	equations[3] = create(11.0d, 3.0d, 'm');
 
-		for (MathEquation equation: equations) {
-			equation.execute();
-			System.out.print("result = ");
-			// System.out.println(equation.result);
-			System.out.println(equation.getResult());
-		}
+		//		for (MathEquation equation: equations) {
+		//			equation.execute();
+		//			System.out.print("result = ");
+		//			// System.out.println(equation.result);
+		//			System.out.println(equation.getResult());
+		//		}
+
+		// Test
+		//		System.out.println();
+		//		System.out.println("Using Overloads");
+		//		System.out.println();
+		//
+		//		double leftDouble = 9.0d;
+		//		double rightDouble = 4.0d;
+		//		int leftInt = 9;
+		//		int rightInt = 4;
+		//
+		//		MathEquation equationOverload = new MathEquation('d');
+		//		equationOverload.execute(leftDouble, rightDouble);
+		//		System.out.print("result = ");
+		//		System.out.println(equationOverload.getResult());
+		//
+		//		equationOverload.execute(leftInt, rightInt);
+		//		System.out.print("result = ");
+		//		System.out.println(equationOverload.getResult());
+		//
+		//		equationOverload.execute((double)leftInt, rightInt);
+		//		System.out.print("result = ");
+		//		System.out.println(equationOverload.getResult());
 
 		// Test
 		System.out.println();
-		System.out.println("Using Overloads");
+		System.out.println("Using Inheritance");
 		System.out.println();
 
-		double leftDouble = 9.0d;
-		double rightDouble = 4.0d;
-		int leftInt = 9;
-		int rightInt = 4;
+		CalculateBase[] calculators = {
+				new Divider(100.0d, 50.0d),
+				new Adder(25.0d, 92.0d),
+				new Subtracter(225.0d, 17.0d),
+				new Multiplier(11.0d, 3.0d)
+		};
 
-		MathEquation equationOverload = new MathEquation('d');
-		equationOverload.execute(leftDouble, rightDouble);
-		System.out.print("result = ");
-		System.out.println(equationOverload.getResult());
-
-		equationOverload.execute(leftInt, rightInt);
-		System.out.print("result = ");
-		System.out.println(equationOverload.getResult());
-
-		equationOverload.execute((double)leftInt, rightInt);
-		System.out.print("result = ");
-		System.out.println(equationOverload.getResult());
+		for (CalculateBase calculator:calculators) {
+			calculator.calculate();
+			System.out.print("result = ");
+			System.out.println(calculator.getResult());
+		}
 
 		// ** Iteration 2 **
 
